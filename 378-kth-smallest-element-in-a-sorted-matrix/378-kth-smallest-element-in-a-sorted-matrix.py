13a940ee8bcdb87539dbeb595548681785a47a59
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-08-02 22:16:08 +0530

    Time: 409 ms (17.09%), Space: 18.7 MB (80.82%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,23 +1,12 @@
-class Solution:  # 160 ms, faster than 93.06%
-    def kthSmallest(self, matrix, k):
+class Solution:  # 204 ms, faster than 54.32%
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         m, n = len(matrix), len(matrix[0])  # For general, the matrix need not be a square
+        minHeap = []  # val, r, c
+        for r in range(min(k, m)):
+            heappush(minHeap, (matrix[r][0], r, 0))
 
-        def countLessOrEqual(x):
-            cnt = 0
-            c = n - 1  # start with the rightmost column
-            for r in range(m):
-                while c >= 0 and matrix[r][c] > x: c -= 1  # decrease column until matrix[r][c] <= x
-                cnt += (c + 1)
-            return cnt
-
-        left, right = matrix[0][0], matrix[-1][-1]
-        ans = -1
-        while left <= right:
-            mid = (left + right) // 2
-            if countLessOrEqual(mid) >= k:
-                ans = mid
-                right = mid - 1  # try to looking for a smaller value in the left side
-            else:
-                left = mid + 1  # try to looking for a bigger value in the right side
-
-        return ans        
+        ans = -1  # any dummy value
+        for i in range(k):
+            ans, r, c = heappop(minHeap)
+            if c+1 < n: heappush(minHeap, (matrix[r][c + 1], r, c + 1))
+        return ans       
