4cc0510c4ac64878aa7e9c749adb48692269e97d
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-08-18 10:50:27 +0530

    Time: 99 ms (49.33%), Space: 91.7 MB (38.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,11 +1,19 @@
-class Solution:
-    def minSetSize(self, arr: List[int]) -> int:
-        cnt = Counter(arr)
-        frequencies = list(cnt.values())
-        frequencies.sort()
-        
-        ans, removed, half = 0, 0, len(arr) // 2
-        while removed < half:
-            ans += 1
-            removed += frequencies.pop()
-        return ans
+class Solution {
+    public int minSetSize(int[] arr) {
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        for (int x : arr) cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+
+        int[] frequencies = new int[cnt.values().size()];
+        int i = 0;
+        for (int freq : cnt.values()) frequencies[i++] = freq;
+        Arrays.sort(frequencies);
+
+        int ans = 0, removed = 0, half = arr.length / 2;
+        i = frequencies.length - 1;
+        while (removed < half) {
+            ans += 1;
+            removed += frequencies[i--];
+        }
+        return ans;
+    }
+}
