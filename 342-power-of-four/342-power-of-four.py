95751493423d04419a642f959dc69439ff04edd9
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-08-22 07:13:52 +0530

    Time: 50 ms (54.31%), Space: 13.8 MB (54.38%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,3 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return n > 0 and not(n & (n-1)) and int(sqrt(n)) * int(sqrt(n)) == n
+        return n > 0 and not(n & (n-1)) and (n-1)%3 == 0
