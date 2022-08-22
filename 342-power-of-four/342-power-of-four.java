267005632f2694307ae5a37576916e6427369a8d
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-08-22 07:05:43 +0530

    Time: 2 ms (65.34%), Space: 40.8 MB (82.08%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        int m=(int)(Math.sqrt(n));
-        for(int i=0;i<=m;i++){
-            if(Math.pow(4,i)==n){
-                return true;
-            }
-        }
-        return false;
+       if(n<=0) return false;
+       if((n&(n-1))!=0) return false;
+        return (n-1)%3==0;
     }
 }
