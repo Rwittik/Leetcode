6edbfd2b9bedc2c7db9493f89f7d0adc488bb7da
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-08-25 08:42:55 +0530

    Time: 13 ms (53.55%), Space: 43 MB (87.14%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,13 +1,32 @@
-class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] alphabets_counter = new int[26];
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         int[] alphabets_counter = new int[26];
         
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
+//         for (char c : magazine.toCharArray())
+//             alphabets_counter[c-'a']++;
 
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) return false;
-            alphabets_counter[c-'a']--;
+//         for (char c : ransomNote.toCharArray()){
+//             if (alphabets_counter[c-'a'] == 0) return false;
+//             alphabets_counter[c-'a']--;
+//         }
+//         for(int i=0;i<26;i++){
+//             System.out.println(alphabets_counter[i]);
+//         }
+//         return true;
+//     }
+// }
+class Solution{
+     public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> magM = new HashMap<>();
+        for (char c:magazine.toCharArray()){
+            int newCount = magM.getOrDefault(c, 0)+1;
+            magM.put(c, newCount);
+        }
+        for (char c:ransomNote.toCharArray()){
+            int newCount = magM.getOrDefault(c,0)-1;
+            if (newCount<0)
+                return false;
+            magM.put(c, newCount);
         }
         return true;
     }
