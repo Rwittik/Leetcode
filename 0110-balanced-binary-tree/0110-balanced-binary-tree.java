b733c28ca691009ead4a1893ca10f211f1a115c3
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-10-16 21:00:55 +0530

    Time: 1 ms (99.20%), Space: 44.1 MB (62.51%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,21 +13,46 @@
  *     }
  * }
  */
+// class Solution {
+//     private boolean result = true;
+
+//     public boolean isBalanced(TreeNode root) {
+//         maxDepth(root);
+//         return result;
+//     }
+
+//     public int maxDepth(TreeNode root) {
+//         if (root == null)
+//             return 0;
+//         int l = maxDepth(root.left);
+//         int r = maxDepth(root.right);
+//         if (Math.abs(l - r) > 1)
+//             result = false;
+//         return 1 + Math.max(l, r);
+//     }
+// }
+
 class Solution {
-    private boolean result = true;
-
     public boolean isBalanced(TreeNode root) {
-        maxDepth(root);
-        return result;
-    }
-
-    public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        int l = maxDepth(root.left);
-        int r = maxDepth(root.right);
-        if (Math.abs(l - r) > 1)
-            result = false;
-        return 1 + Math.max(l, r);
+        // If the tree is empty, we can say it’s balanced...
+        if (root == null)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+    // Create a function to return the “height” of a current subtree using recursion...
+	public int Height(TreeNode root) {
+        // Base case...
+		if (root == null)  return 0;
+        // Height of left subtree...
+		int leftHeight = Height(root.left);
+        // Height of height subtree...
+		int rightHight = Height(root.right);
+        // In case of left subtree or right subtree unbalanced, return -1...
+		if (leftHeight == -1 || rightHight == -1)  return -1;
+        // If their heights differ by more than ‘1’, return -1...
+        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+		return Math.max(leftHeight, rightHight) + 1;
     }
 }
