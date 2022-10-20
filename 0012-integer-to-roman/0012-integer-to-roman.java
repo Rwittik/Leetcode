ac47fd34da046af77eaab0a295e4e3261676b613
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-10-20 10:53:14 +0530

    Time: 4 ms (99.95%), Space: 41.6 MB (99.16%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,25 @@
+public class Solution {
+public String intToRoman(int num) {
+
+    int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i=0;i<values.length;i++) {
+        while(num >= values[i]) {
+            num -= values[i];
+            sb.append(strs[i]);
+        }
+    }
+    return sb.toString();
+ }
+}
+/* 1994=1994-1000->M
+    994=994-900->CM
+    94=94-90->XC
+    4=4-4->IV
+        
+        ^
+     MCMXCIV  
+*/     
