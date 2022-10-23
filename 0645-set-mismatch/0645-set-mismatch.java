6b9e8fb7f883e9729dba38c0bbb65f4c509eb805
Rwittik Dash Adhikary <89292057+Rwittik@users.noreply.github.com>
2022-10-23 10:29:56 +0530

    Time: 14 ms (30.41%), Space: 55 MB (25.59%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,4 +1,13 @@
-class Solution:
-    def findErrorNums(self, nums):
-        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
-        
+class Solution {
+    public static int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
+        for (int i : nums) {
+            if (nums[Math.abs(i) - 1] < 0) res[0] = Math.abs(i);
+        else nums[Math.abs(i) - 1] *= -1;
+        }
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] > 0) res[1] = i+1;
+        }
+        return res;
+    }
+}
